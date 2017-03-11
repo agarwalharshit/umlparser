ecbc369b2d1ffc84c11a4780dae2435320c46bf7
Harshit Agarwal <Harshit@harshit.local>
2017-03-11 03:39:17 -0800

    Add existing file

--- ParserCode/src/UmlParser.java
+++ ParserCode/src/UmlParser.java
@@ -3,7 +3,13 @@ public class UmlParser {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		System.out.println("GettingParser1");
+		
+		  if (args[0].equals("class")) {
+			  System.out.println(args[0]+"  "+args[1]+" "+args[2]);
+	           
+	        } else {
+	            System.out.println("Invalid keyword " + args[0]);
+	        }
 	}
 	
 }
