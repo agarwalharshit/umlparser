2c437807a0538e6613940d1d576361d035386a93
Harshit Agarwal <Harshit@harshit.local>
2017-03-11 15:33:30 -0800

    Add Existing file

--- ParserCode/src/UmlParser.java
+++ ParserCode/src/UmlParser.java
@@ -9,6 +9,7 @@ public class UmlParser {
 	           
 	        }else if (args[0].equals(("seq"))) {
 	        	  System.out.println(args[0]+"  "+args[1]+" "+args[2]);
+	        	  
 	        } 
 		  else {
 	            System.out.println("Invalid keyword " + args[0]);
