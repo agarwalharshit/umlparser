4381cc38d228d9bee891fa9ccd0e82cff1cebd65
Harshit Agarwal <Harshit@harshit.local>
2017-03-18 17:01:08 -0700

    Add Existing

--- ParserCode/src/UmlParser.java
+++ ParserCode/src/UmlParser.java
@@ -4,16 +4,18 @@ public class UmlParser {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		
-		  if (args[0].equals("class")) {
-			  System.out.println(args[0]+"  "+args[1]+" "+args[2]);
-	           
-	        }else if (args[0].equals(("seq"))) {
-	        	  System.out.println(args[0]+"  "+args[1]+" "+args[2]);
-	        } 
-		  else {
-	            System.out.println("Invalid keyword " + args[0]);
-	            System.out.println("Invalid keyword " + args[1]);
-	        }
+//		  if (args[0].equals("class")) {
+//			  System.out.println(args[0]+"  "+args[1]+" "+args[2]);
+//	           
+//	        }else if (args[0].equals(("seq"))) {
+//	        	  System.out.println(args[0]+"  "+args[1]+" "+args[2]);
+//	        } 
+//		  else {
+//	            System.out.println("Invalid keyword " + args[0]);
+//	            System.out.println("Invalid keyword " + args[1]);
+//	        }
+		ParserEngine pe= new ParserEngine();
+		pe.readFileFromFolder();
 	}
 	
 
