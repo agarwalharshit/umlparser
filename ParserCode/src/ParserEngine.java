578c9eb3a0dd677b787e60d7829c778cd83d47bd
Harshit Agarwal <Harshit@harshit.local>
2017-04-08 19:35:01 -0700

    Commiting changes

--- ParserCode/src/ParserEngine.java
+++ ParserCode/src/ParserEngine.java
@@ -76,6 +76,11 @@ public class ParserEngine {
 	    private void buildMap(ArrayList<CompilationUnit> cuArray) {
 	    	for (CompilationUnit cu : cuArray) {
 	    		 List<TypeDeclaration> cl = cu.getTypes();
+	    		 for (Node n : cl) {
+	                 ClassOrInterfaceDeclaration coi = (ClassOrInterfaceDeclaration) n;
+	                 map.put(coi.getName(), coi.isInterface()); // false is class,
+	                                                            // true is interface
+	             }
 		            
 	        }
 	    }
